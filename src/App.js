28b4e13e8432bfff1bb1996cbba9d77d329d908a
Your Name <aileen1125@gmail.com>
2024-01-06 18:25:08 -0800

    base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import Content from './layouts/Content';
-import Footer from './layouts/Footer';
-import Navbar from './layouts/Navbar';
 
 function App() {
   return (
     <div>
-      <Navbar />
-
-      <Content />
-
-      <Footer />
+     
      
     </div>
   );
